55f0c01c7536e624a57ca07591ec30e54d4858ee
Michael G Schwern <mschwern@cpan.org>
2000-08-24 08:15:50 +0000

    Import of MSCHWERN/Dunce-0.01 from CPAN.
    gitpan-cpan-distribution: Dunce
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MSCHWERN/Dunce-0.01.tar.gz
    gitpan-cpan-author:       MSCHWERN
    gitpan-cpan-maturity:     released

---
+++ lib/Dunce/Files.pm
@@ -0,0 +1,223 @@
+package Dunce::Files;
+
+use strict;
+use vars qw($VERSION @EXPORT);
+$VERSION = '0.01';
+
+use base qw(Exporter);
+
+=pod
+
+=head1 NAME
+
+Dunce::Files - Protects against sloppy use of files.
+
+
+=head1 SYNOPSIS
+
+  use Dunce::Files;
+
+  # open() warns you that you forgot to check if it worked.
+  open(FILE, $filename);
+  print <FILE>;
+  exit;
+
+  # *FILE will warn you that you forgot to close it.
+
+
+=head1 DESCRIPTION
+
+One of the most common programming mistakes is failing to check if an
+open() worked.  Same goes for other file and system operations.  The
+world outside your program is a scary, unreliable place, and things
+you try to do with it might not always work.
+
+Dunce::Files makes trick versions of all file functions which do some
+basic sanity checking.  
+
+If used in void context (ie. you didn't check to see if it worked),
+they will throw a warning.  If the function returns a filehandle (like
+open() and readdir()) that filehandle will complain if its never
+closed, or if its never used.
+
+This module is useful for automated code auditing.  Its also useful as
+a dunce cap to place on junior programmers, make sure they're not
+making silly mistakes.
+
+The list of overridden functions is:
+
+             chdir
+             chmod
+             chown
+             chroot
+             dbmopen
+             flock
+             link
+             mkdir
+             open
+             opendir
+             read
+             rename
+             rmdir
+             seek
+             seekdir
+             symlink
+             syscall
+             sysseek
+             system
+             syswrite
+             truncate
+             unlink
+             write
+
+=cut
+
+# Commonly abused file functions.
+@EXPORT = qw(
+             chdir
+             chmod
+             chown
+             chroot
+             dbmopen
+             flock
+             link
+             mkdir
+             open
+             opendir
+             read
+             rename
+             rmdir
+             seek
+             seekdir
+             symlink
+             syscall
+             sysseek
+             syswrite
+             truncate
+             unlink
+             write
+            );
+
+use Function::Override;
+use Carp;
+foreach my $func (@EXPORT) {
+    override($func, sub { 
+                       my $wantarray = (caller(1))[5];
+                       carp "You didn't check if $func() succeeded"
+                         unless defined $wantarray;
+                    }
+            );
+}
+
+=pod
+
+A few functions have some additional warnings:
+
+=over 4
+
+=item B<chmod>
+
+Often, people will gratuitiously grant files more permissions than
+they really need causing unnecessary security problems.  Making
+non-program files executable is a common mistake.  Unnecessarily
+giving world write permission is another.  Dunce::Files will throw a
+warning if either is detected.
+
+I<Note: It may be worthwhile to split this out into a seperate module>
+
+=cut
+
+override('chmod', 
+         sub {
+             my $mode = $_[0];
+             carp "Don't make files executable without a good reason"
+               if $mode & 0111;
+             carp "Don't make files writable by others without a good reason"
+               if $mode & 0003;
+
+             my $wantarray = (caller(1))[5];
+             carp "You didn't check if chmod() succeeded"
+               unless defined $wantarray;
+         }
+        );
+
+=pod
+
+=item B<dbmopen>
+
+dbmopen() will warn you if the hash argument you gave it already
+contains data.
+
+=cut
+
+override('dbmopen',
+         sub {
+             my $hash = $_[0];
+             carp "Hash given to dbmopen() already contains data"
+               if keys %$hash;
+
+             my $wantarray = (caller(1))[5];
+             carp "You didn't check if chmod() succeeded"
+               unless defined $wantarray;
+         }
+        );
+
+=pod
+
+=item B<open>
+
+I<NOT YET IMPLEMENTED>
+
+open() will warn you if you don't close its filehandle explicitly
+before the program ends.  It will also warn if you give it an already
+open filehandle.
+
+XXX I'd also like to have made sure $! is checked, but $! can't be
+usefully tied. :(
+
+=pod
+
+#'#
+# Waiting on postamble callbacks in Function::Override.
+
+=cut
+
+=item B<opendir>
+
+I<NOT YET IMPLEMENTED>
+
+Same as open().
+
+=back
+
+
+=head1 CAVEATS
+
+Because of the way perl compiles, the following code will produce a
+'Name main::FILE used only once: possible typo' where it shouldn't.
+
+    use Dunce::Files;
+    open(FILE, $filename) or die $!;
+    print <FILE>;
+
+Because open() is really Dunce::Files::open() and not the real open,
+Perl doesn't realize that FILE is the filehandle *FILE, so it thinks
+its only being used once.
+
+Turns out this is a useful feature.  If you close FILE the warning
+will go away, and you should have closed it in the first place.
+
+
+=head1 AUTHOR
+
+Michael G Schwern <schwern@pobox.com> with help from crysflame and
+Simon Cozens.
+
+
+=head1 SEE ALSO
+
+L<Function::Override>
+
+=cut
+
+1;
